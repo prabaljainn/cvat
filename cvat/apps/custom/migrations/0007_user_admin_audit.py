7e2acc5d32eef50cadb4e5f380c5d3ad37041e14
yashs33244 <yashs33244@gmail.com>
2026-08-08 00:39:08 +0530

    chore(migrations): renumber user-admin audit migration to 0007

--- cvat/apps/custom/migrations/0007_user_admin_audit.py
+++ cvat/apps/custom/migrations/0007_user_admin_audit.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("custom", "0005_taxonomy_label"),
+        ("custom", "0006_taxonomy_label"),
     ]
 
     operations = [
